ea8f7733809ff926b269b0d583ed488b349ba2bb
usernob <contact.ramzipandu@gmail.com>
2026-03-14 09:44:31 +0700

    feat: add cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,65 @@
+use clap::{Parser, Subcommand};
+
 mod constant;
 mod file;
 mod prime_number;
 mod rsa;
 
-fn main() {}
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Keygen {
+        #[arg(short, long)]
+        output: String,
+
+        #[arg(short, long, default_value = "1024")]
+        bits: usize,
+    },
+
+    Encrypt {
+        #[arg(short, long)]
+        key: String,
+
+        #[arg(short, long)]
+        output: Option<String>,
+
+        input: Option<String>,
+    },
+
+    Decrypt {
+        #[arg(short, long)]
+        key: String,
+
+        #[arg(short, long)]
+        output: Option<String>,
+
+        input: Option<String>,
+    },
+}
+fn main() -> std::io::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Keygen { output, bits } => {
+            let keypair = rsa::keygen(bits as u64);
+            file::save_key(&output, &keypair)?;
+        }
+
+        Commands::Encrypt { key, input, output } => {
+            let pubkey = file::read_public_key(&key)?;
+            rsa::process_encrypt(input.as_deref(), output.as_deref(), &pubkey)?;
+        }
+
+        Commands::Decrypt { key, input, output } => {
+            let privkey = file::read_private_key(&key)?;
+            rsa::process_decrypt(input.as_deref(), output.as_deref(), &privkey)?;
+        }
+    }
+
+    Ok(())
+}
